a9d4c2d91feacf67a7738e5f91ae7af49133ca59
mfahadarif85 <mfahadarif@gmail.com>
2023-01-18 20:38:52 +0500

    POST req in express

--- server.js
+++ server.js
@@ -9,8 +9,38 @@ const friends = [
     id: 0,
     name: "Albert Einstein",
   },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
 ];
 
+//Middleware
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+//express built-in middleware
+app.use(express.json());
+
+//Route Handlers
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Missing friend name",
+    });
+  }
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+  friends.push(newFriend);
+  res.json(friends);
+});
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
