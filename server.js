1f8700f8408f5a62a3627883a3b0f6cf48b079fb
mfahadarif85 <mfahadarif@gmail.com>
2023-01-18 12:59:19 +0500

    route parameters

--- server.js
+++ server.js
@@ -4,8 +4,27 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+const friends = [
+  {
+    id: 0,
+    name: "Albert Einstein",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend doesn't exit",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
