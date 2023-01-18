c902d96fa63591e40ddce6825f26daf2aca7f0d9
mfahadarif85 <mfahadarif@gmail.com>
2023-01-18 21:00:43 +0500

    MVC in express

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require("express");
+const friendsController = require("./controllers/friends.controller");
+const messagesController = require("./controllers/messages.controller");
 
 const app = express();
 
 const PORT = 3000;
-
-const friends = [
-  {
-    id: 0,
-    name: "Albert Einstein",
-  },
-  {
-    id: 1,
-    name: "Sir Isaac Newton",
-  },
-];
 
 //Middleware
 
@@ -28,42 +19,12 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 //Route Handlers
-app.post("/friends", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: "Missing friend name",
-    });
-  }
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  };
-  friends.push(newFriend);
-  res.json(friends);
-});
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
+app.post("/friends", friendsController.postFriend);
+app.get("/friends", friendsController.getFriends);
+app.get("/friends/:friendId", friendsController.getFriend);
 
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({
-      error: "Friend doesn't exit",
-    });
-  }
-});
-
-app.get("/messages", (req, res) => {
-  res.send("<ul><li>Hello Einstein!</li></ul>");
-});
-
-app.post("/messages", (req, res) => {
-  console.log("Updating messages");
-});
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessage);
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}...`);
